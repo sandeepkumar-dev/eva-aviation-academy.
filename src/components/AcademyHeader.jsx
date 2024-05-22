08ae95a128cf69b95b1daf9681bc2d643dca2842
Sandeep Kumar <kr.sandeep2112@gmail.com>
2024-05-22 22:22:30 +0530

    Commit spice-info h1 in css

--- src/components/AcademyHeader.jsx
+++ src/components/AcademyHeader.jsx
@@ -1,83 +1,58 @@
 /* eslint-disable */
 import {useNavigate} from 'react-router-dom';
 import '../styles/AcademyHeader.css';
+
 const AcademyHeader = () => {
   const navigate = useNavigate();
   return (
     <>
       <div className="header">
         <div className="header-1">
-          <img src="./images/logo.jpg" alt="" />
+          <img src="./images/logo.jpg" alt="Eva Aviation Academy Logo" />
           <h1>Eva Aviation Academy</h1>
         </div>
         <ul>
-          <a
-            onClick={() => {
-              navigate('/academyhome');
-            }}
-          >
-            Home
-          </a>
-          <a
-            onClick={() => {
-              navigate('/about');
-            }}
-          >
-            About
-          </a>
-          <a>
-            <div class="dropdown">
-              <span
-                onClick={() => {
-                  navigate('/course');
-                }}
-              >
-                Course
-              </span>
-              <div class="dropdown-content">
-                <a
-                  onClick={() => {
-                    navigate('/ppl-course');
-                  }}
-                >
-                  Private Pilot License (PPL)
-                </a>
-                <a
-                  onClick={() => {
-                    navigate('/cpl-course');
-                  }}
-                >
-                  {' '}
-                  Commercial Pilot License (CPL)
-                </a>
-                <a
-                  onClick={() => {
-                    navigate('/atpl-course');
-                  }}
-                >
-                  Airline Transport Pilot License (ATPL)
-                </a>
+          <li>
+            <a onClick={() => navigate('/academyhome')}>Home</a>
+          </li>
+          <li>
+            <a onClick={() => navigate('/about')}>About</a>
+          </li>
+          <li className="dropdown">
+            <span>Course</span>
+            <div className="dropdown-content">
+              <a onClick={() => navigate('/ppl-course')}>
+                Private Pilot License (PPL)
+              </a>
+              <a onClick={() => navigate('/cpl-course')}>
+                Commercial Pilot License (CPL)
+              </a>
+              <a onClick={() => navigate('/atpl-course')}>
+                Airline Transport Pilot License (ATPL)
+              </a>
+              <div className="nested-dropdown">
+                <span>Type Rating</span>
+                <div className="nested-content">
+                  <a onClick={() => navigate('/type-rating-abc')}>abc</a>
+                  <a onClick={() => navigate('/type-rating-ancnncn')}>
+                    ancnncn
+                  </a>
+                  <a onClick={() => navigate('/type-rating-other')}>Other</a>
+                </div>
               </div>
             </div>
-          </a>
-          <a
-            onClick={() => {
-              navigate('/enquiry');
-            }}
-          >
-            Enquiry
-          </a>
-          <a
-            onClick={() => {
-              navigate('/');
-            }}
-          >
-            MainPage
-          </a>
+          </li>
+          <li>
+            <a onClick={() => navigate('/enquiry')}>Enquiry</a>
+          </li>
+          <li>
+            <a onClick={() => navigate('/')}>MainPage</a>
+          </li>
         </ul>
       </div>
       <hr />
     </>
   );
 };
+
 export default AcademyHeader;
